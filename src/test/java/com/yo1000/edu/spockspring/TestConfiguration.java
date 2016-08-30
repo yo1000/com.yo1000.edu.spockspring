afe395044b74bfccaea64380db312e8a1a627895
yo1000 <yoichi.kikuchi.aug@gmail.com>
2016-08-30 19:52:55 +0900

    Add WordRepository spec

---
+++ src/test/java/com/yo1000/edu/spockspring/TestConfiguration.java
@@ -0,0 +1,19 @@
+package com.yo1000.edu.spockspring;
+
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import com.ninja_squad.dbsetup.destination.Destination;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+/**
+ * @author yo1000
+ */
+@Configuration
+public class TestConfiguration {
+    @Bean
+    public Destination destination(DataSource dataSource) {
+        return new DataSourceDestination(dataSource);
+    }
+}
