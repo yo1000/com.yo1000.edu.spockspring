ade71861dbb48bd16ac09dd6863f53ca8b7c11a4
su-kun1899 <higedrum.coz@gmail.com>
2016-10-10 20:27:28 +0900

    Add history test

--- src/test/groovy/com/yo1000/edu/spockspring/service/WordChainServiceSpec.groovy
+++ src/test/groovy/com/yo1000/edu/spockspring/service/WordChainServiceSpec.groovy
@@ -1,9 +1,44 @@
 package com.yo1000.edu.spockspring.service
 
+import com.yo1000.edu.spockspring.model.Word
+import com.yo1000.edu.spockspring.repository.WordRepository
+import com.yo1000.edu.spockspring.util.WordSplitter
+import org.mockito.Mockito
 import spock.lang.Specification
 import spock.lang.Unroll
+
+import java.text.SimpleDateFormat
 
 @Unroll
 class WordChainServiceSpec extends Specification {
 
+    WordChainService wordChainService
+    WordRepository wordRepository
+
+    def setup() {
+        def wordSplitter = new WordSplitter()
+        wordRepository = Mockito.mock(WordRepository)
+        wordChainService = new WordChainService(wordRepository, wordSplitter)
+    }
+
+    def "履歴取得はリポジトリの返却と一致する"() {
+        setup:
+        Mockito.doReturn(words)
+                .when(wordRepository)
+                .findAllOrderByCreatedDesc()
+
+        expect:
+        words == wordChainService.history()
+
+        where:
+        words | _
+        [w('aaa', '160830'), w('bbb', '160831')] | _
+    }
+
+    def w(String text, String created) {
+        return new Word(
+                text: text,
+                created: new SimpleDateFormat("yyMMdd").parse(created)
+        )
+    }
 }
