0685245fc327f6082ffa57459a67b803d2149236
su-kun1899 <higedrum.coz@gmail.com>
2016-10-10 22:32:19 +0900

    Add repository test

---
+++ src/test/groovy/com/yo1000/edu/spockspring/repository/WordRepositorySpec.groovy
@@ -0,0 +1,39 @@
+package com.yo1000.edu.spockspring.repository
+
+import com.ninja_squad.dbsetup.DbSetup
+import com.ninja_squad.dbsetup.Operations
+import com.ninja_squad.dbsetup.destination.Destination
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import spock.lang.Unroll
+
+
+@Unroll
+@SpringBootTest
+class WordRepositorySpec extends Specification {
+    @Autowired
+    Destination destination
+
+    @Autowired
+    WordRepository repository
+
+    def setup() {
+        def insertWords = Operations.insertInto('WORD')
+                .columns('TEXT', 'CREATED')
+                .values('しりとり', '2016-08-30 19:06:00')
+                .values('りんご', '2016-08-30 19:07:00')
+                .values('ごりら', '2016-08-30 19:08:00')
+                .values('らっぱ', '2016-08-30 19:09:00')
+                .values('ぱいなっぷる', '2016-08-30 19:10:00')
+                .build()
+
+        new DbSetup(destination,
+                Operations.sequenceOf(
+                        Operations.truncate('WORD'),
+                        insertWords
+                )
+        ).launch()
+
+    }
+}
